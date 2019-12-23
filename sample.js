3f6c08047b5177f94206c3e7c7130e2b0aed04ca
fbslee <fbslee@gmail.com>
2019-12-23 09:51:47 -0800

    Add files via upload

--- sample.js
+++ sample.js
@@ -2,6 +2,10 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function navigateTo(location){
+    alert('navigation to ' + location);
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
