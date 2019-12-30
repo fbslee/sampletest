53c5044800d6b31229a8a858bbabebce56a4cfd3
fbslee <fbslee@gmail.com>
2019-12-30 11:10:35 -0800

    Add files via upload

--- sample.js
+++ sample.js
@@ -1,17 +1,87 @@
-function toggleOptions() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
 function navigateTo(location){
-    alert('navigating to ' + window.location.href + ' ' + location);
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-        }
+    let url = "";
+    switch(location){
+        case "home":
+            url = "/Web/user/dashboard/ucd/";
+            break;
+        case "accounts-balance":
+            url = "/Web/account/balances/balances";
+            break;
+        case "accounts-holding":
+            url = "/Web/account/holdings/default/";
+            break;
+        case "accounts-holdingZoom":
+            url = "/Web/account/holdings/holdingZoom/";
+            break;
+        case "accounts-history":
+            url = "/Web/account/history/default/";
+            break;
+        case "accounts-unrealizedGainLoss":
+            url = "/Web/account/unrealizedGainLoss/default/";
+            break;
+        case "accounts-realizedGainLoss":
+            url = "/Web/account/realizedGainLoss/default/";
+            break;
+        case "accounts-asset":
+            url = "/Web/account/assetOverview/allocation";
+            break;
+        case "accounts-download":
+            url = "/Modules/Accounts/Download/enter.php";
+            break;
+        case "trading-order":
+            url = "/Web/account/orders/default/";
+            break;
+        case "research-findSymbol":
+            url = "/Modules/Research/SymbolLookup/symbolLookup.php";
+            break;
+        case "research-optionChain":
+            url = "/Modules/Research/OptionChain/optionChain.php";
+            break;
+        case "research-marketOverview":
+            url = "/Web/morningstar/";
+            break;
+        case "research-advancedChart":
+            url = "/Web/morningstar/chart/";
+            break;
+        case "research-marketsEventCalendar":
+            url = "/Web/morningstar/earningsCalendar/";
+            break;
+        case "research-stockAlerts":
+            url = "/Web/morningstar/alerts/";
+            break;
+        case "research-stockFundamentalsReport":
+            url = "/Web/morningstar/stockReport/";
+            break;
+        case "research-mutualFund":
+            url = "/Web/morningstar/fundReport/";
+            break;
+        case "research-screener":
+            url = "/Web/morningstar/screener/";
+            break;
+        case "calculator-collegeCost":
+            url = "/Modules/Lib/MVC/iframe.php?target=morningstar.collegeCostCalc";
+            break;
+        case "calculator-iraComparison":
+            url = "/Modules/Lib/MVC/iframe.php?target=morningstar.iraCompCalc";
+            break;
+        case "calculator-iraEligibility":
+            url = "/Modules/Lib/MVC/iframe.php?target=morningstar.iraEligCalc";
+            break;
+        case "calculator-iraRothConversion":
+            url = "/Modules/Lib/MVC/iframe.php?target=morningstar.iraRothCalc";
+            break;
+        case "calculator-retirementCost":
+            url = "/Modules/Lib/MVC/iframe.php?target=morningstar.retirementCalc";
+            break;
+        case "tools-messageCenter":
+            url = "/Modules/Communication/MsgCenter/list.php";
+            break;
+        case "logout":
+            url = "/Modules/Login/logoutExpand.php";
+            break;
+        default:
+            url = "/Web/user/dashboard/ucd/";
+            break;
     }
+    alert('what is the url that we are navigating to ' + url);
 }
